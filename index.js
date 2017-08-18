63a47ed13eac2bc530e12f385a3fdde248d864c8
Maksym Bykovskyy <maksym@unbounce.com>
2017-08-18 11:57:42 -0700

    CROW-135 Modified this library to verify a list of domains

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-module.exports = require("./lib/ACMEClient");
+var FS = require("fs");
+var LetsEncryptClient = require("./lib/ACMEClient");
+
+var accountKey = FS.readFileSync("account.key");
+var client = new LetsEncryptClient(accountKey);
+
+client.register()
+.then(() => {
+  var domains = FS.readFileSync("domains-list.txt", "utf8").split("\n");
+
+  domains.reduce((promise, domain) => {
+    return promise.then(() => {
+      return verifyDomain(domain);
+    });
+  }, Promise.resolve());
+})
+.catch((error) => {
+  console.log(error);
+});
+
+function verifyDomain(domain) {
+  return client.requestAuthorization(domain).then((challenge) => {
+      console.log(domain + " OK");
+
+      // Clear pending authorization
+      client.triggerChallenge(challenge);
+  }).catch((error) => {
+      console.log(domain + " " + error.message);
+  });
+}
